fe8094e8b2fd9e042b2402f0aefd30e7d80d53b2
Artem <artem.andreyev212@gmail.com>
2024-04-14 22:07:05 +0300

    Mutation observer addition

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 
 const box = document.querySelector('.box');
 
-let observer = new MutationObserver(MutationRecords => {
-    console.log(MutationRecords);
+let observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords);
 });
+
+observer.observe(box, {
+    childList: true
+});
+
+observer.disconnect();
